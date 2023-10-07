153d1f8c275e538629fbe8d49a19f3deac3aecee
Akash <akash300383@gmail.com>
2023-10-07 22:26:14 +0530

    done

---
+++ JavaScript Programs/arrayDemo.js
@@ -0,0 +1,26 @@
+var num1 = [10,20,30,40,50];            // literal style array declaration with initialization 
+var num2 = new Array(10,20,30,40,50);      // creating memory using new keyword with initialization 
+document.write("<br/> "+num1.length);
+document.write("<br/> "+num2.length);
+// num1.push(60);
+// num1.push(70);      // it add at last 
+// num1.unshift(1);
+// num1.unshift(2);       // it add at begining 
+document.write("<br/>"+num1[0])
+document.write("<br/>"+num1[1])
+document.write("<br/>"+num1);    // it will display in string format. 
+// retreive elements using for loop 
+document.write("<br/> Retreive element one by one using for loop <br/>")
+for(var i=1;i<num1.length;i++){
+    document.write("<br/> "+num1[i]);
+}
+document.write("<br/> Using for in loop <br/>")
+for(var i in num1){
+    document.write("<br/>Index is "+i+" Value is "+num1[i])
+}
+document.write("<br/> Using for of loop <br/>")
+for(var n of num1){
+    document.write("<br/> Value is "+n)
+}
+
+
